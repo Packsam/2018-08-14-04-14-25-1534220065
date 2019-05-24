9d1a60011927d87ae5c427d868060d6944d5d0ee
Packsam <Packsam@yeah.net>
2019-05-24 11:55:51 +0800

    my first version

--- main/main.js
+++ main/main.js
@@ -1,4 +1,22 @@
 module.exports = function main() {
     console.log("Debug Info");
-    return 'Hello World!';
+    var led = [["._.","...","._.","._.","...","._.","._.","._.","._.","._."],["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]];
+    var output = '';
+    var inputnumber = arguments[0];
+    for(i = 0;i<3;i++){
+        for(j = 0;j<arguments[0].length;j++){
+            output += (led[i][inputnumber[j]]);
+            output = AddSeparator(output,j,arguments[0].length-1);
+        }
+    }
+    return output;
 };
+
+function AddSeparator(input,index,length){
+    if(index<length){
+        input+=' ';
+    }else{
+        input+='\n';
+    }
+    return input;
+}
