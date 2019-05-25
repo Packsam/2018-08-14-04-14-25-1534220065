ff20db71c7d34819b7762ca8aa62728f2e93173a
Packsam <Packsam@yeah.net>
2019-05-25 11:18:27 +0800

    input exception

--- main/main.js
+++ main/main.js
@@ -1,7 +1,14 @@
 module.exports = function main() {
     console.log("Debug Info");
-    var led = [["._.","...","._.","._.","...","._.","._.","._.","._.","._."],["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]];
     var output = '';
+    if(arguments.length!=1){
+        output+='Only one input is valid';
+        return output;
+    }else if(isNaN(arguments[0])){
+        output+='Input should be a number';
+        return output;
+    }
+    var led = [["._.","...","._.","._.","...","._.","._.","._.","._.","._."],["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]];
     var inputnumber = arguments[0];
     for(i = 0;i<3;i++){
         for(j = 0;j<arguments[0].length;j++){
