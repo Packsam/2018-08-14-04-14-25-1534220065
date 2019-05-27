377768f63564dd58d8b7f14ddefe96e193319b62
Packsam <Packsam@yeah.net>
2019-05-27 17:47:47 +0800

    encapsulate functions

--- main/main.js
+++ main/main.js
@@ -1,29 +1,42 @@
 module.exports = function main() {
     console.log("Debug Info");
-    var output = '';
-    if(arguments.length!=1){
-        output+='Only one input is valid';
-        return output;
-    }else if(isNaN(arguments[0])){
-        output+='Input should be a number';
-        return output;
+    var myInput = arguments;
+    var myOutput = checkInput(myInput);
+    if(myOutput){
+        return myOutput;
     }
-    var led = [["._.","...","._.","._.","...","._.","._.","._.","._.","._."],["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]];
-    var inputnumber = arguments[0];
+    myOutput = '';
+    var myLed = [["._.","...","._.","._.","...","._.","._.","._.","._.","._."],["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]];
+    var myNumber = myInput[0];
+    myOutput = format(myNumber,myLed,myOutput);
+    return myOutput;
+};
+ 
+
+function checkInput(input){
+    if(input.length!=1){
+        return 'Only one input is valid';
+    }else if(isNaN(input[0])){
+        return 'Input should be a number';
+  
+    }
+}
+
+function format(number,led,output){
     for(i = 0;i<3;i++){
-        for(j = 0;j<arguments[0].length;j++){
-            output += (led[i][inputnumber[j]]);
-            output = AddSeparator(output,j,arguments[0].length-1);
+        for(j = 0;j<number.length;j++){
+            output += (led[i][number[j]]);
+            output = addSeparator(output,j,number.length-1);
         }
     }
     return output;
-};
+}
 
-function AddSeparator(input,index,length){
+function addSeparator(input,index,length){
     if(index<length){
         input+=' ';
     }else{
         input+='\n';
     }
     return input;
-}
+}  
